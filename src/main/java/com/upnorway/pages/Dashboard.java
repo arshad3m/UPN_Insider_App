8e3b7ceb0fdd46d88772876fff25abca7c00a4a4
Roshika <roshikaG@99x.lk>
2019-12-19 15:41:51 +0530

    Added new test cases forChange password from profile

--- src/main/java/com/upnorway/pages/Dashboard.java
+++ src/main/java/com/upnorway/pages/Dashboard.java
@@ -18,23 +18,44 @@ public class Dashboard extends TestBase {
 		goToProfileMenu();
 		click("settings_XPATH");
 	}
-	
+
 	public void enterFirstName(String name) {
-		type("firstName_XPATH",name);
+		type("firstName_XPATH", name);
 	}
-	
+
 	public void enterLastName(String name) {
-		type("lastName_XPATH",name);
+		type("lastName_XPATH", name);
 	}
-	
+
 	public void saveProfile() {
 		click("save_XPATH");
 
 	}
-	
 
 	public void logOut() {
 
 	}
 
+	public void ChangePassword() {
+		click("changePassword_XPATH");
+
+	}
+
+	public void enterCurrentPwd(String pwd) {
+		type("currentPassword_XPATH", pwd);
+	}
+
+	public void enterNewPwd(String pwd) {
+		type("newPassword_XPATH", pwd);
+	}
+
+	public void enterConfirmPwd(String pwd) {
+		type("confirmPassword_XPATH", pwd);
+	}
+
+	public void changePwd() {
+click("buttonChangepwd_XPATH");
+	}
+
+	
 }
--- src/test/java/com/upnorway/testcases/Dashboard_test.java
+++ src/test/java/com/upnorway/testcases/Dashboard_test.java
@@ -1,7 +1,8 @@
 package com.upnorway.testcases;
 
+import java.io.IOException;
 import java.util.Hashtable;
-
+import org.openqa.selenium.By;
 import org.testng.SkipException;
 import org.testng.annotations.Test;
 
@@ -10,11 +11,9 @@ import com.upnorway.base.TestBase;
 import com.upnorway.pages.Dashboard;
 
 public class Dashboard_test extends TestBase {
-	
-	
-	Dashboard db= new Dashboard();
-	
-	
+
+	Dashboard db = new Dashboard();
+
 	@Test(enabled = true, dataProviderClass = TestUtil.class, dataProvider = "dp", priority = 1)
 	public void changeName_happyPath(Hashtable<String, String> data) {
 
@@ -22,30 +21,72 @@ public class Dashboard_test extends TestBase {
 
 			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
 		}
-		
-		
+
 		db.goToMyProfile();
-		db.enterFirstName(data.get("fname"));
+		db.enterFirstName(data.get("fname")); // b.enterFirstName("Gihan")
 		db.enterLastName(data.get("lname"));
 		db.saveProfile();
 	}
-	
-	
+
 	@Test(enabled = true, dataProviderClass = TestUtil.class, dataProvider = "dp", priority = 2)
 	public void profileChangeValidations(Hashtable<String, String> data) {
-		
+
 		if (!data.get("runmode").equals("Y")) {
 
 			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
 		}
-		
+
 		db.goToMyProfile();
 		db.enterFirstName(data.get("fname"));
 		db.enterLastName(data.get("lname"));
 		db.saveProfile();
 	}
 	
-	
+	@Test(enabled = true, dataProviderClass = TestUtil.class, dataProvider = "dp", priority = 3)
+	public void changePasswordHappyPath(Hashtable<String, String> data) throws IOException, Exception {
+
+		if (!data.get("runmode").equals("Y")) {
+
+			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
+		}
+
+		db.goToMyProfile();
+		db.ChangePassword();
+		db.enterCurrentPwd(data.get("currentpwd"));
+		db.enterNewPwd(data.get("newpwd"));
+		db.enterConfirmPwd(data.get("confirmpwd"));
+		db.changePwd();
+		
+		Thread.sleep(3000);
+
+		String validationMessage = driver.findElement(By.xpath(OR.getProperty("cpSuccessmessage_XPATH"))).getText();;
+
+		verifyEquals(validationMessage, data.get("message"));
+
+	}
+
+	@Test(enabled = true, dataProviderClass = TestUtil.class, dataProvider = "dp", priority = 4)
+	public void checkChangePasswordValidation(Hashtable<String, String> data) throws IOException, Exception {
+
+		if (!data.get("runmode").equals("Y")) {
+
+			throw new SkipException("Skipping the test case as the Run mode for data set is NO");
+		}
+
+		db.goToMyProfile();
+		db.ChangePassword();
+		db.enterCurrentPwd(data.get("currentpwd"));
+		db.enterNewPwd(data.get("newpwd"));
+		db.enterConfirmPwd(data.get("confirmpwd"));
+		db.changePwd();
+		
+		Thread.sleep(3000);
+
+		String validationMessage = driver.findElement(By.xpath(OR.getProperty("cpValidationMessage_XPATH"))).getText();;
+
+		verifyEquals(validationMessage, data.get("message"));
+
+	}
 	
 	
 
